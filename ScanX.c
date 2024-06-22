028923ef0f54ce68a33a5af342550f69c7a95bc7
progprnv <pranavpranav29907@gmail.com>
2024-06-22 15:34:20 +0530

    Initial Release

---
+++ ScanX.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+
+{ int rows,cols;
+	printf("enter the number of rows");
+	scanf("%d",&rows);
+	
+	printf("enter the number of columns");
+	scanf("%d",&cols);
+	
+	int matrix1[i][j];
+	
+	printf("enter the elements of matrix");
+	for (i=0;i<rows;i++)
+	{ for (j=0;j<cols;j++)
+		{ scanf("%d",&matrix1[i][j];
+		}
+	}
+	
+	//transpsoe
+	
+	printf("transpose of a matrix");
+	for (int j=0;j<cols;j++)
+	{ for (int i=0;i<rows;i++)
+		{ printf("%d",matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+	
